1bb4afaa236752cf5b3f935ec543e92eef480213
sachin kumar <merugune.sachin123@gmail.com>
2021-07-06 00:09:31 +0530

    inbuild number functions are answered

---
+++ inbuildN.js
@@ -0,0 +1,44 @@
+var num = 100
+console.log(toString(num))
+
+console.log(num.toString())
+
+
+var num1 = 44
+console.log(parseInt(num1))
+
+
+var num2 = 22.222
+console.log(parseInt(num2))
+
+
+var name = 'Banana'
+console.log(parseInt(name))
+
+
+var num3 = 12
+console.log(parseFloat(num3))
+
+
+var num4 = 13.3
+console.log(parseFloat(num4))
+
+
+var num5 = 10
+console.log(num5.toFixed(2))
+
+
+var num6 = 15.456789
+console.log(num6.toFixed())
+
+
+var num7 = 15.456789
+console.log(num7.toFixed(2))
+
+
+var num8 = 15.456789
+console.log(num7.toFixed(3))
+
+
+var num9 = 15.456789
+console.log(num9.toFixed(4))
